6467912edeb914506506cfb5fad9f5d27054b048
Jinho Kim <jinho.kim@fau.de>
2023-02-02 14:25:20 +0100

    Add read_config()

--- utils.py
+++ utils.py
@@ -11,7 +11,6 @@ def get_category_id(categories, category_name):
             return [category['id']]  # since tag_ids are stored in the list!!
 
 
-
 def get_tag_id(tags, tag_names):
     tag_ids = []
     for tag_name in tag_names:
@@ -36,3 +35,7 @@ def get_tag_webItemID(tags, tag_ids):
                 web_ids.append(tag['properties']['Webflow item ID']['rich_text'][0]['text']['content'])
 
     return web_ids
+
+
+def read_config(path):
+    return json.load(open(path))
